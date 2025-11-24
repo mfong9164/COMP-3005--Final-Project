9579a46da50bccde7c9c409da4b0d61b6496ecf2
Neil Varshney <neil4varshney@gmail.com>
2025-11-23 20:32:29 -0500

    Created U.I files. Created a view

--- app/main.py
+++ app/main.py
@@ -24,7 +24,11 @@ from models.trainer import Trainer
 from models.equipment import Equipment
 from models.maintenance_ticket import MaintenanceTicket
 from models.participates_in import ParticipatesIn
-from app.sample_data import getSampleData
+# from app.sample_data import getSampleData
+from models.paid_date_trigger import create_paid_date_trigger
+from models.unpaid_bills_view import create_unpaid_bills_view, drop_unpaid_bills_view
+
+from ui import main_menu
 
 
 DB_USER = 'postgres'
@@ -32,54 +36,25 @@ DB_HOST = 'localhost'
 DB_PORT = '5432'
 DB_NAME = 'Health and Fitness Club Management System'
 
-def create_paid_date_trigger(engine):
-    # create the trigger function. only set paid_date if paid changed from False to True. if paid is set back to False, clear the paid_date
-    function_sql = """
-    CREATE OR REPLACE FUNCTION set_paid_date()
-    RETURNS TRIGGER AS $$
-    BEGIN
-        IF OLD.paid = FALSE AND NEW.paid = TRUE THEN
-            NEW.paid_date = CURRENT_DATE;
-        END IF;
-        
-        IF NEW.paid = FALSE THEN
-            NEW.paid_date = NULL;
-        END IF;
-        
-        RETURN NEW;
-    END;
-    $$ LANGUAGE plpgsql;
-    """
-
-    ## create the trigger, and drop it if it already exists to prevent errors. Before updating the bill, we will check if
-    ## the paid column has changed. if it has, we run the trigger function to set the paid date.
-    trigger_sql = """
-    DROP TRIGGER IF EXISTS trigger_set_paid_date on "Bill";
-
-    CREATE TRIGGER trigger_set_paid_date
-    BEFORE UPDATE ON "Bill"
-    FOR EACH ROW
-    WHEN (OLD.paid IS DISTINCT FROM NEW.paid)
-    EXECUTE FUNCTION set_paid_date();
-    """
-
-    with engine.connect() as conn:
-        conn.execute(text(function_sql))
-        conn.execute(text(trigger_sql))
-        conn.commit()
-        print("Trigger 'trigger_set_paid_date' created successfully")
 
 def create_connection():
     try:
         DB_PASSWORD = getpass("DB Password: ")
         engine = create_engine(f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
+
         with engine.connect() as conn:
             print("Connected")
+
+            drop_unpaid_bills_view(engine)
+
             Base.metadata.drop_all(engine, checkfirst=True)
             Base.metadata.create_all(engine, checkfirst=True)
 
             # we create the trigger after tables are created
             create_paid_date_trigger(engine)
+
+            # create the view after tables are created
+            create_unpaid_bills_view(engine)
 
     except Exception as e:
         print(f"Failed: {e}")
@@ -87,17 +62,21 @@ def create_connection():
 
     with Session(engine) as session:
         try:
-            sample_data = getSampleData()
-            session.add_all(sample_data)
-            User_email = input("User Email: ")
-            if (session.execute(select(Member.email).where(Member.email == User_email)) == User_email):
-                print("member login")
+            pass
+            # sample_data = getSampleData()
+            # session.add_all(sample_data)
+            # User_email = input("User Email: ")
+            # if (session.execute(select(Member.email).where(Member.email == User_email)) == User_email):
+            #     print("member login")
         except Exception as e:
             print(f"Failed: {e}")
             quit()
+    
+    return engine
 
 if __name__ == '__main__':
-    create_connection()
+    engine = create_connection()
+    main_menu(engine)
     
 
 
---
+++ app/ui.py
@@ -0,0 +1,100 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from models.member import Member
+from models.trainer import Trainer
+from models.admin import Admin
+
+def main_menu(engine):
+    # main menu to select user role
+    while True:
+        print("\n=== Health & Fitness Club Management System ===")
+        print("1. Member")
+        print("2. Trainer")
+        print("3. Admin")
+        print("4. Exit")
+        
+        choice = input("Select option: ")
+        
+        if choice == "1":
+            member_menu(engine)
+        elif choice == "2":
+            trainer_menu(engine)
+        elif choice == "3":
+            admin_menu(engine)
+        elif choice == "4":
+            break
+
+
+def member_menu(engine):
+
+    while True:
+        print("\n=== Member Menu ===")
+        print("1. Member Login")
+        print("2. Member Registration")
+        print("3. Back to Main Menu")
+        
+        choice = input("Select option: ")
+        
+        if choice == "1":
+            member_login(engine)
+
+        elif choice == "2":
+            member_registration(engine)
+
+        elif choice == "3":
+            break  # Go back to main menu
+        else:
+            print("Invalid option. Please try again.")
+
+
+def member_login(engine):
+    member_email = input("Enter your email: ")
+
+    with Session(engine) as session:
+        try:
+            member = session.query(Member).filter_by(email=member_email).first()
+
+            if member:
+                print(f"Login Successfully. Welcome {member.name}!")
+                member_dashboard(engine, member_email)
+            
+            else:
+                print("Member not found. Please enter correct email or register.")
+            
+        except Exception as e:
+            print(f"Error: {e}")
+
+def member_dashboard(engine, member_email):
+    # member dashboard
+    while True:
+
+        ## add more if needed
+        print("\n=== Member Dashboard ===")
+        print("1. Update Personal Details")
+        print("2. Update Health History")
+        print("3. Update Fitness Goals")
+        print("4. Logout")
+        
+        choice = input("Select option: ")
+        
+        if choice == "1":
+            update_member_profile(engine, member_email)
+        elif choice == "2":
+            log_member_health_metrics(engine, member_email)
+        elif choice == "3":
+            print("Logged out. Returning to Member Menu...")
+            break  # Goes back to member_menu
+        else:
+            print("Invalid option. Please try again.")
+
+def update_member_profile():
+    pass
+
+
+def trainer_menu(engine):
+    # trainer UI code here
+    pass
+
+def admin_menu(engine):
+    # admin UI code here
+    pass
